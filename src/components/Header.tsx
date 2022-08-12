f287d63cf5e707f56735af643f3894ca25e89efe
pedro-aparelhotv <pedro@aparelhotv.com>
2022-08-11 22:55:26 -0300

    feat(menu): adding menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,16 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 const Header = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+  const handleOpenMenu = () => {
+    setMenuIsOpen(true)
+  }
+  const handleCloseMenu = () => {
+    setMenuIsOpen(false)
+  }
+
   return (
     <header className="header">
       <div className="header__wrapper">
@@ -8,7 +18,88 @@ const Header = () => {
           <a className="header__link">Maaretta Jaukkuri Foundation</a>
         </Link>
 
-        <button className="header__menu-btn">Menu</button>
+        <button
+          className="header__menu-btn"
+          type="button"
+          onClick={handleOpenMenu}
+        >
+          Menu
+        </button>
+
+        <div className="header__menu" data-open={menuIsOpen}>
+          <div className="header__menu__wrapper">
+            <div className="header__map-lines">
+              <div className="header__map-lines__column" />
+              <div className="header__map-lines__column" />
+              <div className="header__map-lines__column" />
+              <div className="header__map-lines__column" />
+            </div>
+
+            <nav className="header__menu__nav">
+              <ul className="header__menu__list">
+                <li className="header__menu__item" data-id="foundation">
+                  <Link href="/" passHref>
+                    <a className="header__menu__link">
+                      <span>01</span> The Foundation
+                    </a>
+                  </Link>
+                </li>
+                <li className="header__menu__item" data-id="mission">
+                  <Link href="/" passHref>
+                    <a className="header__menu__link">
+                      <span>02</span> Mission
+                    </a>
+                  </Link>
+                </li>
+                <li className="header__menu__item" data-id="fellows">
+                  <Link href="/" passHref>
+                    <a className="header__menu__link">
+                      <span>03</span> Fellows
+                    </a>
+                  </Link>
+                </li>
+                <li className="header__menu__item" data-id="about">
+                  <Link href="/" passHref>
+                    <a className="header__menu__link">
+                      <span>04</span> About
+                    </a>
+                  </Link>
+                </li>
+                <li className="header__menu__item" data-id="library">
+                  <Link href="/" passHref>
+                    <a className="header__menu__link">
+                      <span>05</span> Library
+                    </a>
+                  </Link>
+                </li>
+                <li className="header__menu__item" data-id="events">
+                  <Link href="/" passHref>
+                    <a className="header__menu__link">
+                      <span>06</span> Events
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="header__menu__footer">
+              <div className="header__menu__footer__group">
+                <div className="header__menu__tag">
+                  <span>winter</span>
+                </div>
+                <a className="header__menu__tag">instagram</a>
+              </div>
+
+              <button
+                className="header__close-btn"
+                type="button"
+                onClick={handleCloseMenu}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   )
---
+++ src/pages/library.tsx
@@ -0,0 +1,19 @@
+const Library = () => {
+  return (
+    <main className="library">
+      <div className="library__wrapper">
+        <h1 className="library__title">
+          <span>The Maaretta Jaukkuri Foundation – MJF –</span> invites 4-5
+          <span>Fellows</span> each year from various fields in the arts,
+          sciences or philosophy, or{' '}
+          <span>any field that encourages new paths of thinking</span> . The
+          Fellows are selected by the Board to spend up to 2 months in{' '}
+          <span>The Place in Lofoten</span>. MJF also presents public seminars
+          and other <span>events</span>.
+        </h1>
+      </div>
+    </main>
+  )
+}
+
+export default Library
